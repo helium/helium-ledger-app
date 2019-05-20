de0e34ed44728e25bb43ff0f1ea75dcc90b31a0a
TamtamHero <10632523+TamtamHero@users.noreply.github.com>
2019-05-20 15:37:21 +0200

    Add some depandancies and warnings

--- prepare-devenv.sh
+++ prepare-devenv.sh
@@ -1,16 +1,29 @@
+# shellcheck disable=SC1091,SC2155
+
 # SOURCE THIS FILE
 # . prepare-devenv blue|s|x
 
 if [ $# -ne 1 ]; then
     echo "Possible options: blue, s or x"
     return
-elif [ "-h" == $1 ]; then
+elif [[ $1 == "-h" ]]; then
     echo "Possible options: blue, s or x"
     return
-elif [ "blue" != $1 ] && [ "s" != $1 ] && [ "x" != $1 ]; then
+elif [[ $1 != "blue" ]] && [[ $1 != "s" ]] && [[ $1 != "x" ]]; then
     echo "Possible options: blue, s or x"
     return
 fi
+
+if [[ $(dpkg-query -s python3-venv 2>&1) == *'is not installed'* ]]; then
+    printf "\nPackage python3-venv is missing.\nOn Debian-like distros, run:\n\napt install python3-venv\n\n"
+    return
+fi
+
+if [[ $(cat /etc/udev/rules.d/20-hw1.rules) == *'ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004"'* ]]; then
+    printf "\nMissing udev rules. Please refer to https://support.ledger.com/hc/en-us/articles/115005165269-Fix-connection-issues\n\n"
+    return
+fi
+
 
 if [ ! -d dev-env ]; then
     mkdir dev-env
@@ -36,34 +49,31 @@ if [ ! -d dev-env ]; then
     mv clang+llvm* dev-env/CC/nanox/clang-arm-fropi
 
     wget https://github.com/LedgerHQ/blue-secure-sdk/archive/blue-r21.1.tar.gz -O blue-secure-sdk.tar.gz
-    tar xf blue-secure-sdk.tar.gz 
+    tar xf blue-secure-sdk.tar.gz
     rm blue-secure-sdk.tar.gz
     mv blue-secure-sdk* dev-env/SDK/blue-secure-sdk
 
     wget https://github.com/LedgerHQ/nanos-secure-sdk/archive/nanos-1553.tar.gz -O nanos-secure-sdk.tar.gz
-    tar xf nanos-secure-sdk.tar.gz 
+    tar xf nanos-secure-sdk.tar.gz
     rm nanos-secure-sdk.tar.gz
     mv nanos-secure-sdk* dev-env/SDK/nanos-secure-sdk
 
     python3 -m venv dev-env/ledger_py3
     source dev-env/ledger_py3/bin/activate
+    pip install wheel
     pip install ledgerblue
-
 fi
 
 
 source dev-env/ledger_py3/bin/activate
 
-if [ "blue" == $1 ]; then
+if [[ $1 == "blue" ]]; then
     export BOLOS_SDK=$(pwd)/dev-env/SDK/blue-secure-sdk
     export BOLOS_ENV=$(pwd)/dev-env/CC/others
-elif [ "s" == $1 ]; then
+elif [[ $1 == "s" ]]; then
     export BOLOS_SDK=$(pwd)/dev-env/SDK/nanos-secure-sdk
     export BOLOS_ENV=$(pwd)/dev-env/CC/others
-elif [ "x" == $1 ]; then
+elif [[ $1 == "x" ]]; then
     export BOLOS_SDK=$(pwd)/dev-env/SDK/nanox-secure-sdk
     export BOLOS_ENV=$(pwd)/dev-env/CC/nanox
 fi
-    
-    
-
