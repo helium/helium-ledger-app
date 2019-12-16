5bc1593a359845e9926ef7bc8e9d3df2a153265f
Raf <raphael.geslain@ledger.fr>
2019-12-16 17:41:19 +0100

    [Flow on nano S] - Inclusion of 'ux.h' and suppression of target-dependent UX sources.

--- src/globals.c
+++ src/globals.c
@@ -1,17 +1,11 @@
 #include "globals.h"
 #include "os.h"
-
-#ifdef TARGET_NANOX
 #include "ux.h"
+
 ux_state_t G_ux;
 bolos_ux_params_t G_ux_params;
-#else // TARGET_NANOX
-ux_state_t ux;
-#endif // TARGET_NANOX
 
 // display stepped screens
 unsigned int ux_step;
 unsigned int ux_step_count;
-
-
 const internalStorage_t N_storage_real;
--- src/menu.c
+++ src/menu.c
@@ -4,88 +4,13 @@
 volatile uint8_t dummy_setting_1;
 volatile uint8_t dummy_setting_2;
 
-#if defined(TARGET_NANOS)
-
-#ifdef HAVE_U2F
-
-static const ux_menu_entry_t menu_main[];
-static const ux_menu_entry_t menu_settings[];
-static const ux_menu_entry_t menu_dummy_setting_1[];
-static const ux_menu_entry_t menu_dummy_setting_2[];
-
-// change the setting
-static void menu_dummy_setting_1_change(unsigned int enabled) {
-    dummy_setting_1 = enabled;
-    nvm_write(&N_storage.dummy_setting_1, (void*)&dummy_setting_1, sizeof(uint8_t));
-    // go back to the menu entry
-    UX_MENU_DISPLAY(0, menu_settings, NULL);
-}
-
-static void menu_dummy_setting_2_change(unsigned int enabled) {
-    dummy_setting_2 = enabled;
-    nvm_write(&N_storage.dummy_setting_2, (void*)&dummy_setting_2, sizeof(uint8_t));
-    // go back to the menu entry
-    UX_MENU_DISPLAY(0, menu_settings, NULL);
-}
-
-// show the currently activated entry
-static void menu_dummy_setting_1_init(unsigned int ignored) {
-    UNUSED(ignored);
-    UX_MENU_DISPLAY(N_storage.dummy_setting_1?1:0, menu_dummy_setting_1, NULL);
-}
-
-static void menu_dummy_setting_2_init(unsigned int ignored) {
-    UNUSED(ignored);
-    UX_MENU_DISPLAY(N_storage.dummy_setting_2?1:0, menu_dummy_setting_2, NULL);
-}
-
-static const ux_menu_entry_t menu_dummy_setting_1[] = {
-    {NULL, menu_dummy_setting_1_change, 0, NULL, "No", NULL, 0, 0},
-    {NULL, menu_dummy_setting_1_change, 1, NULL, "Yes", NULL, 0, 0},
-    UX_MENU_END
-};
-
-static const ux_menu_entry_t menu_dummy_setting_2[] = {
-    {NULL, menu_dummy_setting_2_change, 0, NULL, "No", NULL, 0, 0},
-    {NULL, menu_dummy_setting_2_change, 1, NULL, "Yes", NULL, 0, 0},
-    UX_MENU_END
-};
-
-static const ux_menu_entry_t menu_settings[] = {
-    {NULL, menu_dummy_setting_1_init, 0, NULL, "Dummy setting 1", NULL, 0, 0},
-    {NULL, menu_dummy_setting_2_init, 0, NULL, "Dummy setting 2", NULL, 0, 0},
-    {menu_main, NULL, 1, &C_icon_back, "Back", NULL, 61, 40},
-    UX_MENU_END
-};
-#endif // HAVE_U2F
-
-static const ux_menu_entry_t menu_about[] = {
-    {NULL, NULL, 0, NULL, "Version", APPVERSION , 0, 0},
-    {menu_main, NULL, 2, &C_icon_back, "Back", NULL, 61, 40},
-    UX_MENU_END
-};
-
-static const ux_menu_entry_t menu_main[] = {
-    //{NULL, NULL, 0, &NAME3(C_nanos_badge_, CHAINID, ), "Use wallet to", "view accounts", 33, 12},
-    {NULL, NULL, 0, NULL, "Boilerplate", "ready", 0, 0},
-    {menu_settings, NULL, 0, NULL, "Settings", NULL, 0, 0},
-    {menu_about, NULL, 0, NULL, "About", NULL, 0, 0},
-    {NULL, os_sched_exit, 0, &C_icon_dashboard, "Quit app", NULL, 50, 29},
-    UX_MENU_END
-};
-
-#elif defined(TARGET_NANOX)
-
 void display_settings(void);
 void switch_dummy_setting_1_data(void);
 void switch_dummy_setting_2_data(void);
 
 //////////////////////////////////////////////////////////////////////
-
-
 const char* settings_submenu_getter(unsigned int idx);
 void settings_submenu_selector(unsigned int idx);
-
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Enable contract data submenu:
@@ -224,17 +149,10 @@ UX_FLOW(ux_idle_flow,
   FLOW_LOOP
 );
 
-
-#endif
-
 void ui_idle(void) {
-#if defined(TARGET_NANOS)
-    UX_MENU_DISPLAY(0, menu_main, NULL);
-#elif defined(TARGET_NANOX)
     // reserve a display stack slot if none yet
     if(G_ux.stack_count == 0) {
         ux_stack_push();
     }
     ux_flow_init(0, ux_idle_flow, NULL);
-#endif // #if TARGET_ID
 }
