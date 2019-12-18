90cb9506808d42c5e0ba08bc898bad5418157a94
Louis Thiery <louis@helium.com>
2019-12-17 16:01:56 -0800

    Update copyright

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 *
 *  (c) 2016 Ledger
 *  (c) 2018 Nebulous
+*  (c) 2019 Helium
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
@@ -15,92 +16,6 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 ********************************************************************************/
-
-// This code also serves as a walkthrough for writing your own Ledger Nano S
-// app. Begin by reading this file top-to-bottom, and proceed to the next file
-// when directed. It is recommended that you install this app on your Nano S
-// so that you can see how each section of code maps to real-world behavior.
-// This also allows you to experiment by modifying the code and observing the
-// effect on the app.
-//
-// I'll begin by describing the high-level architecture of the app. The entry
-// point is this file, main.c, which initializes the app and runs the APDU
-// request/response loop. The loop reads APDU packets from the computer, which
-// instructs it to run various commands. The Sia app supports three commands,
-// each defined in a separate file: getPublicKey, signHash, and calcTxnHash.
-// These each make use of Sia-specific functions, which are defined in sia.c.
-// Finally, some global variables and helper functions are declared in ux.h.
-//
-// Each command consists of a command handler and a set of screens. Each
-// screen has an associated set of elements that can be rendered, a
-// preprocessor that controls which elements are rendered, and a button
-// handler that processes user input. The command handler is called whenever
-// sia_main receives an APDU requesting that command, and is responsible for
-// displaying the first screen of the command. Control flow then moves to the
-// button handler for that screen, which selects the next screen to display
-// based on which button was pressed. Button handlers are also responsible for
-// sending APDU replies back to the computer.
-//
-// The control flow can be a little confusing to understand, because the
-// button handler isn't really on the "main execution path" -- it's only
-// called via interrupt, typically while execution is blocked on an
-// io_exchange call. (In general, it is instructive to think of io_exchange as
-// the *only* call that can block.) io_exchange exchanges APDU packets with
-// the computer: first it sends a response packet, then it receives a request
-// packet. This ordering may seem strange, but it makes sense when you
-// consider that the Nano S has to do work in between receiving a command and
-// replying to it. Thus, the packet sent by io_exchange is a *response* to the
-// previous request, and the packet received is the next request.
-//
-// But there's a problem with this flow: in most cases, we can't respond to
-// the command request until we've received some user input, e.g. approving a
-// signature. If io_exchange is the only call that blocks, how can we tell it
-// to wait for user input? The answer is a special flag, IO_ASYNC_REPLY. When
-// io_exchange is called with this flag, it blocks, but it doesn't send a
-// response; instead, it just waits for a new request. Later on, we make a
-// separate call to io_exchange, this time with the IO_RETURN_AFTER_TX flag.
-// This call sends the response, and then returns immediately without waiting
-// for the next request. Visually, it is clear that these flags have opposite
-// effects on io_exchange:
-//
-//                                      ----Time--->
-//    io_exchange:        [---Send Response---|---Wait for Request---]
-//    IO_ASYNC_REPLY:                           ^Only do this part^
-//    IO_RETURN_AFTER_TX:  ^Only do this part^
-//
-// So a typical command flow looks something like this. We start in sia_main,
-// which is an infinite loop that starts by calling io_exchange. It receives
-// an APDU request from the computer and calls the associated command handler.
-// The handler displays a screen, e.g. "Generate address?", and sets the
-// IO_ASYNC_REPLY flag before returning. Control returns to sia_main, which
-// loops around and calls io_exchange again; due to the flag, it now blocks.
-// Everything is frozen until the user decides which button to press. When
-// they eventually press the "Approve" button, the button handler jumps into
-// action. It generates the address, constructs a response APDU containing
-// that address, calls io_exchange with IO_RETURN_AFTER_TX, and redisplays the
-// main menu. When a new command arrives, it will be received by the blocked
-// io_exchange in sia_main.
-//
-// More complex commands may require multiple requests and responses. There
-// are two approaches to handling this. One approach is to treat each command
-// handler as a self-contained unit: that is, the main loop should only handle
-// the *first* request for a given command. Subsequent requests are handled by
-// additional io_exchange calls within the command handler. The other approach
-// is to let the main loop handle all requests, and design the handlers so
-// that they can "pick up where they left off." Both designs have tradeoffs.
-// In the Sia app, the only handler that requires multiple requests is
-// calcTxnHash, and it takes the latter approach.
-//
-// On the other end of the spectrum, there are simple commands that do not
-// require any user input. Many Nano S apps have a "getVersion" command that
-// replies to the computer with the app's version. In this case, it is
-// sufficient for the command handler to prepare the response APDU and allow
-// the main loop to send it immediately, without setting IO_ASYNC_REPLY.
-//
-// The important things to remember are:
-// - io_exchange is the only blocking call
-// - the main loop invokes command handlers, which display screens and set button handlers
-// - button handlers switch between screens and reply to the computer
 
 #include <stdint.h>
 #include <stdbool.h>
