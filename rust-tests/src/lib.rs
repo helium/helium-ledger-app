2dbe6589106918ab6accc756e7ec01525c123d41
Tyera Eulberg <tyera@solana.com>
2020-02-11 18:37:12 -0700

    Add rust pubkey and simple signing tests

---
+++ rust-tests/src/lib.rs
@@ -0,0 +1,125 @@
+#[cfg(test)]
+mod tests {
+    use serial_test_derive::serial;
+    use solana_remote_wallet::remote_wallet::{
+        initialize_wallet_manager, DerivationPath, RemoteWallet,
+    };
+    use solana_sdk::{
+        pubkey::Pubkey, signature::Signature, system_instruction, transaction::Transaction,
+    };
+    use std::{collections::HashSet, str::FromStr};
+
+    #[test]
+    #[serial]
+    fn ledger_pubkey_test() {
+        let wallet_manager = initialize_wallet_manager();
+
+        // Update device list
+        wallet_manager.update_devices().expect("No Ledger found, make sure you have a unlocked Ledger connected with the Ledger Wallet Solana running");
+        assert!(wallet_manager.list_devices().len() > 0);
+
+        // Fetch the base pubkey of a connected ledger device
+        let ledger_base_pubkey = wallet_manager
+            .list_devices()
+            .iter()
+            .filter(|d| d.manufacturer == "ledger".to_string())
+            .nth(0)
+            .map(|d| d.pubkey.clone())
+            .expect("No ledger device detected");
+
+        let ledger = wallet_manager
+            .get_ledger(&ledger_base_pubkey)
+            .expect("get device");
+
+        let mut pubkey_set = HashSet::new();
+        pubkey_set.insert(ledger_base_pubkey);
+
+        let pubkey_0_0 = ledger
+            .get_pubkey(&DerivationPath {
+                account: 0,
+                change: Some(0),
+            })
+            .expect("get pubkey");
+        pubkey_set.insert(pubkey_0_0);
+        let pubkey_0_1 = ledger
+            .get_pubkey(&DerivationPath {
+                account: 0,
+                change: Some(1),
+            })
+            .expect("get pubkey");
+        pubkey_set.insert(pubkey_0_1);
+        let pubkey_1 = ledger
+            .get_pubkey(&DerivationPath {
+                account: 1,
+                change: None,
+            })
+            .expect("get pubkey");
+        pubkey_set.insert(pubkey_1);
+        let pubkey_1_0 = ledger
+            .get_pubkey(&DerivationPath {
+                account: 1,
+                change: Some(0),
+            })
+            .expect("get pubkey");
+        pubkey_set.insert(pubkey_1_0);
+
+        assert_eq!(pubkey_set.len(), 5); // Ensure keys at various derivation paths are unique
+    }
+
+    /// This test requires interactive approval of message signing on the ledger.
+    #[test]
+    #[serial]
+    fn ledger_sign_transaction_test() {
+        let wallet_manager = initialize_wallet_manager();
+
+        // Update device list
+        wallet_manager.update_devices().expect("No Ledger found, make sure you have a unlocked Ledger connected with the Ledger Wallet Solana running");
+        assert!(wallet_manager.list_devices().len() > 0);
+
+        // Fetch the base pubkey of a connected ledger device
+        let ledger_base_pubkey = wallet_manager
+            .list_devices()
+            .iter()
+            .filter(|d| d.manufacturer == "ledger".to_string())
+            .nth(0)
+            .map(|d| d.pubkey.clone())
+            .expect("No ledger device detected");
+
+        let ledger = wallet_manager
+            .get_ledger(&ledger_base_pubkey)
+            .expect("get device");
+
+        let derivation_path = DerivationPath {
+            account: 12345,
+            change: None,
+        };
+
+        let from = ledger.get_pubkey(&derivation_path).expect("get pubkey");
+        let instruction = system_instruction::transfer(&from, &ledger_base_pubkey, 42);
+        let transaction = Transaction::new_unsigned_instructions(vec![instruction]);
+        let message_data = transaction.message_data();
+        let signature = ledger
+            .sign_transaction(&derivation_path, transaction)
+            .expect("sign transaction");
+        assert!(signature.verify(&from.as_ref(), &message_data));
+
+        // Test large transaction
+        let recipients: Vec<(Pubkey, u64)> = (0..4).map(|_| (Pubkey::new_rand(), 42)).collect();
+        let instructions = system_instruction::transfer_many(&from, &recipients);
+        let transaction = Transaction::new_unsigned_instructions(instructions);
+        let message_data = transaction.message_data();
+        let signature = ledger
+            .sign_transaction(&derivation_path, transaction)
+            .expect("sign transaction");
+        assert!(signature.verify(&from.as_ref(), &message_data));
+
+        // Test hex string message
+        let data = hex::decode("5ca1ab1e").expect("decode hex");
+
+        let result = ledger
+            .sign_raw_data(&derivation_path, &data)
+            .expect("send apdu");
+        let signature = Signature::new(&result);
+        assert_eq!(signature, Signature::from_str("4hTVyB3wwrsKH91FpzCxzE5hHFNub2sFEcj1M6kL1u5pDc8wMdX4vdt4qjdDvKBUvmurUp9eskcmhARZnbxU7D6v").unwrap());
+    }
+}
