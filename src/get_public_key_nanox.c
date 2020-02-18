afc4a43564888b60f0139f8291d760ed5b34c071
Pierre Collet <pierre.collet@ledger.fr>
2020-02-18 15:58:32 +0100

    Fix displaying of pubkey on screen when not asked

--- src/get_public_key_nanox.c
+++ src/get_public_key_nanox.c
@@ -51,23 +51,25 @@ void handle_get_public_key(uint8_t p1, uint8_t p2, uint8_t *dataBuffer, uint16_t
 	cx_sha256_t hash;
 	unsigned char hash_buffer[32];
 
-	cx_sha256_init(&hash);
-	cx_hash(&hash.header, CX_LAST, G_io_apdu_buffer, adpu_tx, hash_buffer, 32);
-	cx_sha256_init(&hash);
-	cx_hash(&hash.header, CX_LAST, hash_buffer, 32, hash_buffer, 32);
+	if (p1 == P1_PUBKEY_DISPLAY_ON) {
+	  cx_sha256_init(&hash);
+	  cx_hash(&hash.header, CX_LAST, G_io_apdu_buffer, adpu_tx, hash_buffer, 32);
+	  cx_sha256_init(&hash);
+	  cx_hash(&hash.header, CX_LAST, hash_buffer, 32, hash_buffer, 32);
 
-	os_memmove(&G_io_apdu_buffer[adpu_tx], hash_buffer, SIZE_OF_SHA_CHECKSUM);
-	adpu_tx += SIZE_OF_SHA_CHECKSUM;
+	  os_memmove(&G_io_apdu_buffer[adpu_tx], hash_buffer, SIZE_OF_SHA_CHECKSUM);
+	  adpu_tx += SIZE_OF_SHA_CHECKSUM;
 
-	// Encoding key as a base58 string
-	btchip_encode_base58(G_io_apdu_buffer, adpu_tx, ctx->fullStr, &output_len);
-	ctx->fullStr[51] = '\0';
+	  // Encoding key as a base58 string
+	  btchip_encode_base58(G_io_apdu_buffer, adpu_tx, ctx->fullStr, &output_len);
+	  ctx->fullStr[51] = '\0';
 
-	// Running the flow showing the pubkey in the screen
-	ui_getPublicKey();
+	  // Running the flow showing the pubkey in the screen
+	  ui_getPublicKey();
 
 	
-	*flags |= IO_ASYNCH_REPLY;
+	  *flags |= IO_ASYNCH_REPLY;
+	}
 
 	// Flush the APDU buffer, sending the response.
 	io_exchange_with_code(SW_OK, adpu_tx);
