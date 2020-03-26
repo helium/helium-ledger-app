83b158939452ec25616e949893d488fe84e910d9
Trent Nelson <trent@solana.com>
2020-03-25 18:41:10 -0600

    Add tests for `process_message_body` (#58)

---
+++ libsol/message_test.c
@@ -0,0 +1,46 @@
+#include "message.c"
+#include "sol/printer.h"
+#include <assert.h>
+#include <stdio.h>
+
+#define ARRAY_LEN(a) (sizeof(a) / sizeof((a)[0]))
+void test_process_message_body_ok() {
+    Pubkey accounts[] = {
+        {{171, 88, 202, 32, 185, 160, 182, 116, 130, 185, 73, 48, 13, 216, 170, 71, 172, 195, 165, 123, 87, 70, 130, 219, 5, 157, 240, 187, 26, 191, 158, 218}},
+        {{204, 241, 115, 109, 41, 173, 110, 48, 24, 113, 210, 213, 163, 78, 1, 112, 146, 114, 235, 220, 96, 185, 184, 85, 163, 27, 124, 48, 54, 250, 233, 54}},
+        {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+    };
+    Blockhash blockhash = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+    MessageHeader header = {{1, 0, 1, 3}, accounts, &blockhash, 1};
+    uint8_t msg_body[] = {2, 2, 0, 1, 12, 2, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0};
+    field_t fields[5];
+    size_t fields_used = 0;
+    assert(process_message_body(msg_body, ARRAY_LEN(msg_body), &header, fields, &fields_used) == 0);
+    assert(fields_used == 4);
+}
+
+void test_process_message_body_too_many_ix_fail() {
+    MessageHeader header = {{0, 0, 0, 0}, NULL, NULL, 2};
+    assert(process_message_body(NULL, 0, &header, NULL, NULL) == 1);
+}
+
+void test_process_message_body_too_short_ix_fail() {
+    MessageHeader header = {{0, 0, 0, 0}, NULL, NULL, 1};
+    assert(process_message_body(NULL, 0, &header, NULL, NULL) == 1);
+}
+
+void test_process_message_body_bad_ix_account_index_fail() {
+    MessageHeader header = {{0, 0, 0, 1}, NULL, NULL, 1};
+    uint8_t msg_body[] = {1, 0, 0};
+    assert(process_message_body(msg_body, ARRAY_LEN(msg_body), &header, NULL, NULL) == 1);
+}
+
+int main() {
+    test_process_message_body_ok();
+    test_process_message_body_too_many_ix_fail();
+    test_process_message_body_too_short_ix_fail();
+    test_process_message_body_bad_ix_account_index_fail();
+
+    printf("passed\n");
+    return 0;
+}
