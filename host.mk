5563ed256100919d5e7795b4affd53405d53d1ad
Leopold Schabel <leo@leoluk.de>
2020-04-30 19:07:39 +0200

    Pin Python dependencies for install.sh (#120)

--- host.mk
+++ host.mk
@@ -13,23 +13,31 @@ endif
 include config.min
 include $(BOLOS_SDK)/Makefile.defines
 
+.PHONY: load
 load:
 	python3 -m ledgerblue.loadApp $(APP_LOAD_PARAMS)
 
+.PHONY: load-offline
 load-offline:
 	python3 -m ledgerblue.loadApp $(APP_LOAD_PARAMS) --offline
 
+.PHONY: delete
 delete:
 	python3 -m ledgerblue.deleteApp $(COMMON_DELETE_PARAMS)
 
-release:
-	@echo "#!/usr/bin/env bash" > install.sh
-	@echo "cat <<EOF >> app.hex" >> install.sh
-	@cat bin/app.hex >> install.sh
-	@echo "EOF" >> install.sh
+.PHONY: release
+release: install.sh
+
+install.sh: host.mk requirements.txt bin/app.hex
+	echo > install.sh
+
+	export APP_CODE="$$(cat bin/app.hex)"; \
+	export APP_REQUIREMENTS="$$(cat requirements.txt)"; \
 	export APP_LOAD_PARAMS_EVALUATED="$(shell printf '\\"%s\\" ' $(APP_LOAD_PARAMS:bin/%=%))"; \
-	cat install-template.sh | envsubst >> install.sh
+	cat install-template.sh | envsubst '$$APP_LOAD_PARAMS_EVALUATED $$APP_CODE $$APP_REQUIREMENTS' >> install.sh
+
 	chmod +x install.sh
 
+.PHONY: deps
 deps:
-	python3 -mpip install ledgerblue
+	python3 -mpip install -r requirements.txt --require-hashes
--- install-template.sh
+++ install-template.sh
@@ -1,4 +1,25 @@
-python3 -m venv ledger-env
-ledger-env/bin/pip3 install ledgerblue
-ledger-env/bin/python3 -m ledgerblue.loadApp $APP_LOAD_PARAMS_EVALUATED
-rm -rf ledger-env app.hex
+#!/bin/bash
+set -euo pipefail
+
+TMPDIR=$(mktemp -d)
+trap 'rm -rf "$TMPDIR"' EXIT
+
+(  
+  cd "$TMPDIR"
+
+  cat <<'EOF' > app.hex
+$APP_CODE
+EOF
+  
+  cat <<'EOF' > requirements.txt
+$APP_REQUIREMENTS
+EOF
+  
+  python3 -m venv ledger-env
+  ledger-env/bin/pip3 install --require-hashes -r requirements.txt
+  ledger-env/bin/python3 -m ledgerblue.loadApp $APP_LOAD_PARAMS_EVALUATED
+)
+  
+
+
+
