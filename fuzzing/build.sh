3acf82e5329b2af512957c971d25f442f8228a75
Nicolas Iooss <nicolas.iooss@ledger.fr>
2021-05-11 09:48:04 +0200

    fuzzing: make the shell scripts more robust
    * Quote variables to prevent issues when running the scripts in a path
      which contains spaces.
    * Use `set -e` in order to make the script fail as soon as a command
      fails.
    * Add `"$@"` in `./run.sh` to easily run the fuzzer with options such as
      `-jobs=4` (which launch 4 instances of the fuzzer).

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 SCRIPTDIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
-BUILDDIR=$SCRIPTDIR/cmake-build-fuzz
+BUILDDIR="$SCRIPTDIR/cmake-build-fuzz"
 
 # Compile fuzzer
 rm -rf "$BUILDDIR"
 mkdir "$BUILDDIR"
-cd "$BUILDDIR" || exit
+cd "$BUILDDIR"
 
 cmake -DCMAKE_C_COMPILER=clang ..
 make clean
--- fuzzing/run.sh
+++ fuzzing/run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BUILDDIR=$SCRIPTDIR/cmake-build-fuzz
-CORPUSDIR=$SCRIPTDIR/corpus
+set -e
 
-"$BUILDDIR"/fuzz_message "$CORPUSDIR" > /dev/null
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUILDDIR="$SCRIPTDIR/cmake-build-fuzz"
+CORPUSDIR="$SCRIPTDIR/corpus"
+
+"$BUILDDIR"/fuzz_message "$CORPUSDIR" "$@" > /dev/null
