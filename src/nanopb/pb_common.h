9615d3ad2b8e35065354684de888bdbcf205e6e0
lthiery <thiery.louis@gmail.com>
2022-01-25 12:43:33 -0800

    update nanopb

--- src/nanopb/pb_common.h
+++ src/nanopb/pb_common.h
@@ -18,6 +18,12 @@ bool pb_field_iter_begin(pb_field_iter_t *iter, const pb_msgdesc_t *desc, void *
 /* Get a field iterator for extension field. */
 bool pb_field_iter_begin_extension(pb_field_iter_t *iter, pb_extension_t *extension);
 
+/* Same as pb_field_iter_begin(), but for const message pointer.
+ * Note that the pointers in pb_field_iter_t will be non-const but shouldn't
+ * be written to when using these functions. */
+bool pb_field_iter_begin_const(pb_field_iter_t *iter, const pb_msgdesc_t *desc, const void *message);
+bool pb_field_iter_begin_extension_const(pb_field_iter_t *iter, const pb_extension_t *extension);
+
 /* Advance the iterator to the next field.
  * Returns false when the iterator wraps back to the first field. */
 bool pb_field_iter_next(pb_field_iter_t *iter);
@@ -25,6 +31,15 @@ bool pb_field_iter_next(pb_field_iter_t *iter);
 /* Advance the iterator until it points at a field with the given tag.
  * Returns false if no such field exists. */
 bool pb_field_iter_find(pb_field_iter_t *iter, uint32_t tag);
+
+/* Find a field with type PB_LTYPE_EXTENSION, or return false if not found.
+ * There can be only one extension range field per message. */
+bool pb_field_iter_find_extension(pb_field_iter_t *iter);
+
+#ifdef PB_VALIDATE_UTF8
+/* Validate UTF-8 text string */
+bool pb_validate_utf8(const char *s);
+#endif
 
 #ifdef __cplusplus
 } /* extern "C" */
